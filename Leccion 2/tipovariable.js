7032c04370f55d2dcfe0d436bbdf7b0cb3fbffae
Elmer-Fuentes <delacruzelmer27@gmail.com>
2025-06-11 15:08:10 -0600

    Tipo de datos

---
+++ Leccion 2/tipovariable.js
@@ -0,0 +1,14 @@
+var nombre = "Elmer De La Cruz";
+console.log(nombre);
+
+let numero = 5000;
+alert(numero);
+
+var objeto = {
+    nombre: "Elmer De La Cruz",
+    edad: 25,
+    Quetzales: 10.00,
+    Telefono: 123142312
+}
+
+console.log(objeto)
