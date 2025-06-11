20c52f629e0b891a1e4e580879f167ae7676cf80
Elmer-Fuentes <delacruzelmer27@gmail.com>
2025-06-11 15:16:58 -0600

    Uso de typeof para saber que tipo de datos se almacena en var

---
+++ Leccion 3/typeof.js
@@ -0,0 +1,14 @@
+var nombre = "Elmer De La Cruz";
+console.log(typeof nombre);
+
+let numero = 5000;
+alert(typeof numero);
+
+var objeto = {
+    nombre: "Elmer De La Cruz",
+    edad: 25,
+    Quetzales: 10.00,
+    Telefono: 123142312
+}
+
+console.log(typeof objeto)
