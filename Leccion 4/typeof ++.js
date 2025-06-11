3077e8df55026d71da75a52d9b98abfb6ff5f5e7
Elmer-Fuentes <delacruzelmer27@gmail.com>
2025-06-11 15:55:22 -0600

    typeof ++ clase y constructores, objetos

---
+++ Leccion 4/typeof ++.js
@@ -0,0 +1,33 @@
+//valores boleanos
+var condicion = false;
+console.log(typeof condicion);
+
+//funcion
+function mifuncion(){}
+console.log(typeof mifuncion);
+
+//clase 
+class objetoclase{
+
+    Contructor (nombre, edad,moneda)
+    {
+        this.nombre = nombre
+        this.edad = edad
+        this.moneda = 120.1
+
+    }
+}
+
+console.log(typeof objetoclase)
+
+
+//tipo dato indefinido
+var x;
+console.log(typeof x)
+
+var y = undefined;
+console.log(typeof y)
+
+
+var yx = null;
+console.log(typeof yx);
